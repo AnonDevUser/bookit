0a3d7f0cf7a010472ad3e1d706db5bf701a316c1
AarenNepal <nepalaaren63@gmail.com>
2025-12-27 22:25:43 +0545

    (ADD): Intiial Database Design

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import UserProfile, Booking, BookingItem, BookingType
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Booking)
+admin.site.register(BookingType)
+admin.site.register(BookingItem)
--- book/models.py
+++ book/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.user.username
+
+
+class BookingType(models.Model):
+    name = models.CharField(max_length=20) 
+
+    def __str__(self):
+        return self.name
+
+
+class BookingItem(models.Model):
+    type = models.ForeignKey(BookingType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    price_per_hour = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    item = models.ForeignKey(BookingItem, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user} → {self.item}"
